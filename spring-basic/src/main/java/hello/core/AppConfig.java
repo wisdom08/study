ec8eadd2ca87f8982dc59a1cb7ebeca5ae16db61
wisdom08 <wisdom08@kakao.com>
2023-02-19 14:33:14 +0900

    feat: `AppConfig` 리팩토링
    - 중복 제거
    - 역할과 구현 클래스 분리

--- spring-basic/src/main/java/hello/core/AppConfig.java
+++ spring-basic/src/main/java/hello/core/AppConfig.java
@@ -10,11 +10,18 @@ import hello.core.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    private MemoryMemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    private FixDiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
+    }
 }
