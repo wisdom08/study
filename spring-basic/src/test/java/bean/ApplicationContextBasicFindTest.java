d2596bcfcf0544c106d45240c3c4c6e8309456a1
wisdom08 <wisdom08@kakao.com>
2023-02-21 02:47:10 +0900

    test: 스프링 컨테이너에서 스프링 빈을 조회하는 관련 연습용 테스트 코드를 추가합니다

---
+++ spring-basic/src/test/java/bean/ApplicationContextBasicFindTest.java
@@ -0,0 +1,45 @@
+package bean;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회") // 유연성이 떨어지는 코드
+    void findBeanByConcreteType() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("존재하지 않는 빈 이름으로 조회해서 실패한다.")
+    void findBeanByUnknownName() {
+        Assertions.assertThrowsExactly(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("actionService", MemberService.class));
+    }
+}
