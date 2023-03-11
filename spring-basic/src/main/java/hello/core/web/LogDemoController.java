781982a600868f7934b1d8a7f123f6844f3b9e5d
wisdom08 <wisdom08@kakao.com>
2023-03-11 12:15:39 +0900

    feat: 프록시 방법을 이용해서 request 스코프 빈이 생성됐을 때 진짜 `mylogger.logic()`을 호출합니다

--- spring-basic/src/main/java/hello/core/web/LogDemoController.java
+++ spring-basic/src/main/java/hello/core/web/LogDemoController.java
@@ -2,7 +2,6 @@ package hello.core.web;
 
 import hello.core.common.MyLogger;
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,18 +10,21 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LogDemoController {
 
     private final LogDemoService logDemoService;
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
-    public LogDemoController(LogDemoService logDemoService, ObjectProvider<MyLogger> myLoggerProvider) {
+    public LogDemoController(LogDemoService logDemoService, MyLogger myLogger) {
         this.logDemoService = logDemoService;
-        this.myLoggerProvider = myLoggerProvider;
+        this.myLogger = myLogger;
     }
 
     @RequestMapping("log-demo")
     @ResponseBody
     public String logDemo(HttpServletRequest request) {
-        MyLogger myLogger = myLoggerProvider.getObject();
+
         String requestURL = request.getRequestURL().toString();
+
+        System.out.println("myLogger.getClass() = " + myLogger.getClass());
+
         myLogger.setRequestURL(requestURL);
 
         myLogger.log("controller test");
--- spring-basic/src/main/java/hello/core/web/LogDemoService.java
+++ spring-basic/src/main/java/hello/core/web/LogDemoService.java
@@ -1,20 +1,19 @@
 package hello.core.web;
 
 import hello.core.common.MyLogger;
-import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LogDemoService {
 
-    private final ObjectProvider<MyLogger> myLoggerProvider;
+    private final MyLogger myLogger;
 
-    public LogDemoService(ObjectProvider<MyLogger> myLoggerProvider) {
-        this.myLoggerProvider = myLoggerProvider;
+    public LogDemoService(MyLogger myLogger) {
+        this.myLogger = myLogger;
     }
 
+
     public void logic(String id) {
-        MyLogger myLogger = myLoggerProvider.getObject();
         myLogger.log("service id = " + id);
     }
 }
